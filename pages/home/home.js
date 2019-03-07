7629a50d4691dcfe430ced08390dda3f4ea6292d
JonMarkov <553636995@qq.com>
2019-03-07 15:03:17 +0800

    登录页面完成/首页开始进行

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,252 +1,83 @@
+// 声明接口名称
+var WxLicUrl = getApp().globalData.wx_url_1;
 Page({
-
-  /**
-   * 页面的初始数据
-   */
+  // SJ页面初始数据 页面所需数据定义
   data: {
-    homeImg: [
-      {
+    // 初始化UserId数据
+    user_id: '',
+    // 初始化轮播banner数据
+    homeImg: [{
         'imgUrl': 'https://pic4.zhimg.com/v2-3da78883263a03fe9b14b6f72e88e0de_1200x500.jpg'
       },
       {
         'imgUrl': 'https://pic1.zhimg.com/80/v2-0cfa27275a933e6ab81ec1097a24d25c_hd.jpg'
+      },
+      {
+        'imgUrl': 'https://pic4.zhimg.com/v2-3da78883263a03fe9b14b6f72e88e0de_1200x500.jpg'
       }
     ],
-    homeCom: [],
+    homeCom: [{
+
+    }],
     frequency: [],
     nullHouse: true, //先设置隐藏
     swiperCurrent: 0,
+    // 当前显示banner的索引
     currentSwiper: 0
   },
-  goWeimeng: function() {
-    wx.navigateTo({
-      url: 'outWeimeng/outWeimeng',
-    })
-  },
+  // DY函数定义 实时监控当前显示的banner图索引函数
   swiperChange: function(e) {
     this.setData({
       currentSwiper: e.detail.current
     })
   },
+  // DY函数定义 点击banner图函数
   ruleshow: function() {
     var currentSwiper = this.data.currentSwiper
-    if (currentSwiper==1){
-      wx.navigateTo({
-        url: 'phoneCharge/phoneCharge',
-      })
+    if (currentSwiper == 0) {
+      console.log('等于0')
     }
-    if (currentSwiper==0){
-      var that = this;
-      this.setData({
-        nullHouse: false, //弹窗显示
-      })
-      setTimeout(function () {
-        that.data.nullHouse = true
-      }, 1000)
-    }else{
-      console.log(currentSwiper)
-    }
-
-  },
-  ruleHide: function() {
-    this.setData({
-      nullHouse: true, //弹窗关闭
-    })
-  },
-
-
-  bindToCharge(e) {
-    //e是事件源 e.currentTarget点击的目标
-    var chickId = e.currentTarget.dataset.chick;
-    if (chickId == 5) {
-      wx.navigateTo({
-        url: 'phoneCharge/phoneCharge'
-      })
-    } else if (chickId == 8) {
-      url: "mall/mall?frequency_id=" + chickId
+    if (currentSwiper == 1) {
+      console.log('等于1')
     } else {
-      wx.navigateTo({
-        url: "mall/mall?frequency_id=" + chickId
-      })
+      console.log('等于2')
     }
+
   },
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  // DY函数定义 请求banner轮播图函数
+  reqBanner: function() {
+    console.log(this.data.user_id)
+  },
+  // SMZQ生命周期钩子函数 监听页面加载
   onLoad: function(options) {
-    console.log(options)
-    var that = this;
+    // 重定向this指向，预防回调函数改变this问题
+    var _this = this;
+    // 获取本地缓存内的user_id信息
     wx.getStorage({
-      key: "uerid",
+      key: 'loginInfo',
       success: function(res) {
-        that.setData({
-          user_id: res.data
+        // 把user_id存入本地data数据
+        _this.setData({
+          user_id: res.data.userId
         })
-      }
+      },
     })
-    //设置数据到data中
   },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
+  // SMZQ生命周期钩子函数 监听页面初次渲染完成
   onReady: function() {
-    var that = this;
-    wx.getStorage({
-      key: "uerid",
-      success: function(res) {
-        that.setData({
-          user_id: res.data
-        })
-      }
-    })
-    //获取公共接口
-    var phoneUrl = getApp().globalData.wx_url_1
-    //执行初始化事件函数
-    this.HomeRequest(phoneUrl, this.homeCallback)
-
+    //ZX函数执行 请求banner轮播图函数
+    this.reqBanner()
   },
-  // 初始化页面请求 遍历数据函数
-  HomeRequest: function(url, callback) {
-    wx.request({
-      url: url,
-      method: "POST",
-      data: {
-        service: "home",
-        channel: '2'
-      },
-      header: {
-        "content-type": "application/json"
-      },
-      success: function(res) {
-        callback(res.data)
-      }
-    })
-  },
-  // 初始化页面请求 遍历数据函数的回调函数
-  homeCallback: function(res) {
-    console.log(res)
-    //轮播接口参数
-    var bannerList = res.banner_list;
-    var homeImg = [];
-    // 商品列表参数
-    var comList = res.goods_list;
-    var homeCom = [];
-    // 循环轮播参数
-    for (var idx in bannerList) {
-      // 轮播图
-      var imgUrl = bannerList[idx].imgUrl;
-      // id参数
-      var id = bannerList[idx].id;
-      var temp = {
-        imgUrl: imgUrl,
-        id: id,
-      }
-      homeImg.push(temp)
-    };
-    // 循环商品参数
-    for (var idx in comList) {
-      var goods_id = comList[idx].goods_id;
-      //广告词
-      var ad_words = comList[idx].ad_words;
-      // 商品name
-      var goods_name = comList[idx].goods_name;
-      // 商品原价
-      var price = comList[idx].price;
-      //商品现价
-      var sale_price = comList[idx].sale_price
-      //商品介绍图
-      var pic_list = comList[idx].pic_list[0].picUrl
-      //skuid
-      var sku_id = comList[idx].sku_id
-
-      var temp = {
-        ad_words: ad_words,
-        goods_name: goods_name,
-        price: price,
-        sale_price: sale_price,
-        pic_list: pic_list,
-        goods_id: goods_id,
-        sku_id: sku_id
-      }
-      homeCom.push(temp)
-    }
-    var frequencyList = res.frequency_list;
-    var frequency = [];
-    for (var i in frequencyList) {
-      var frequencyName = frequencyList[i].name;
-      var frequencyId = frequencyList[i].id;
-      var frequencyImg = frequencyList[i].picUrl;
-
-      var temp = {
-        frequencyName: frequencyName,
-        frequencyId: frequencyId,
-        frequencyImg: frequencyImg
-      }
-      frequency.push(temp)
-    };
-    this.setData({
-      homeImg: homeImg,
-      homeCom: homeCom,
-      frequency: frequency
-    })
-  },
-  //点击进入详情页面
-  bindToModity: function(e) {
-    var goods_id = e.currentTarget.dataset['modity'];
-    var sku_id = e.currentTarget.dataset['sku']
-    wx.navigateTo({
-      url: "/pages/home/commodityDet/commodityDet?goods_id=" + goods_id + "&" + "sku_id=" + sku_id
-    })
-  },
-  bindToMall: function(e) {
-
-    wx.navigateTo({
-      url: "mall/mall"
-    })
-  },
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-
-  onShareAppMessage: function(res) {
-    // return {
-    //   title:'转多鱼',
-    //   path: 'pages/login/login?sharrUserId=' + this.data.user_id
-    // }
-  },
+  // SMZQ生命周期个钩子函数 监听页面显示
+  onShow: function() {},
+  // SMZQ生命周期钩子函数 监听页面隐藏
+  onHide: function() {},
+  // SMZQ生命周期钩子函数 监听页面卸载
+  onUnload: function() {},
+  // SMZQ生命周期钩子函数 监听用户下拉动作
+  onPullDownRefresh: function() {},
+  // SMZQ生命周期钩子函数 页面上拉触底事件的处理函数
+  onReachBottom: function() {},
+  // SMZQ生命周期钩子函数 用户点击右上角分享
+  onShareAppMessage: function(res) {},
 })
--- pages/login/login.js
+++ pages/login/login.js
@@ -1,16 +1,27 @@
+// 声明接口名称
+var WxLicUrl = getApp().globalData.wx_url_1;
 Page({
+  // SJ页面初始数据 页面所需数据定义
   data: {
     //判断小程序的API，回调，参数，组件等是否在当前版本可用。
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
+  // SMZQ生命周期钩子函数 首先触发onLoad方法，一个页面只会调用一次。
   onLoad: function(options) {
-    var that = this;
+    // 重定向this指向，预防回调函数改变this问题
+    var _this = this;
+    // ZX函数执行 执行判断是否已经授权
+    _this.gaveNotExpired()
+  },
+  // DY函数定义 判断用户是否已经授权，如果已授权则直接跳转
+  gaveNotExpired:function(){
     // 查看是否授权
     wx.getSetting({
-      success: function(res) {
+      success: function (res) {
         if (res.authSetting['scope.userInfo']) {
+          // 获取用户信息
           wx.getUserInfo({
-            success: function(res) {
+            success: function (res) {
               //如果用户已经授权过则直接跳转首页
               wx.switchTab({
                 url: '/pages/home/home'
@@ -21,26 +32,33 @@ Page({
       }
     })
   },
-// 点击按钮开始授权
+  // DY函数定义 授权点击按钮开始授权
   bindGetUserInfo: function(e) {
+    // 获取用户的信息
     if (e.detail.userInfo) {
+      // 把用户的信息存入本地缓存
       wx.setStorage({
         key: "weChat",
         data: e.detail.userInfo
       })
+      // 开始请求登录授权接口
       wx.login({
         success: res => {
+          // 获取返回的code之后请求Java后台
+          // 拼装请求所需参数
           var params = {
-            service: 'userVerify',
-            code: res.code,
-            nickName: e.detail.userInfo.nickName,
+            // 请求方法名
+            action: 'login',
+            // 请求参数
+            requestParam: {
+              code: res.code
+            }
           }
-          params.service = 'userVerify'
-          params.code = res.code
-          const newparams = Object.assign(params, getApp().globalData.params);
-          // const newparams = util.sign(params1)
+          // 请求参数组合
+          const newparams = Object.assign(params);
+          // 请求登录的Java后台接口
           wx.request({
-            url: getApp().globalData.wx_url_1,
+            url: WxLicUrl,
             data: newparams,
             method: "POST",
             // dataType: JSON,
@@ -48,24 +66,24 @@ Page({
               'content-type': 'application/json' // 默认值
             },
             success: res => {
-              // 把用户id设置到本地缓存
+              console.log(res)
+              // 把返回的数据存进本地缓存
               wx.setStorage({
-                key: "uerid",
-                data: res.data.user_id
+                key: "loginInfo",
+                data: res.data
               })
-              //从数据库获取用户信息
-              var macid = res.data.user_id
-              this.phoneHttp(macid, this.phoneHttpCallback)
+              // 点击允许按钮之后如果code等于0则代表请求成功->跳转Home页面
+              if (res.data.code == 0){
+                wx.switchTab({
+                  url: '/pages/home/home'
+                })
+              }
             }
           })
         }
       });
-      // 点击之后跳转
-      wx.switchTab({
-        url: '/pages/home/home'
-      })
     } else {
-      //用户按了拒绝按钮
+      //如果用户按了拒绝按钮
       wx.showModal({
         title: '警告',
         content: '您点击了拒绝授权，将无法进入小程序，请授权之后再进入!!!',
