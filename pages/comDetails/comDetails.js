8c709472c2180b16351bd903059027ed0b0d5a2f
JonMarkov <553636995@qq.com>
2019-03-08 20:09:01 +0800

    UI图基本画完

--- pages/comDetails/comDetails.js
+++ pages/comDetails/comDetails.js
@@ -1,4 +1,5 @@
 // pages/comDetails/comDetails.js
+var WxLicUrl = getApp().globalData.wx_url_1;
 Page({
 
   /**
@@ -7,60 +8,119 @@ Page({
   data: {
 
   },
-
+  
+  reqBanner: function() {
+    var _this = this
+    // 声明user_id
+    var user_id = this.data.user_id
+    // 拼装请求所需参数
+    var params = {
+      // 请求方法名
+      action: 'getAdvertByState',
+      // 请求参数
+      requestParam: {
+        user_id: user_id
+      }
+    }
+    // 请求参数合并
+    const newparams = Object.assign(params);
+    // 请求登录的Java后台接口
+    wx.request({
+      url: WxLicUrl,
+      data: newparams,
+      method: "POST",
+      // dataType: JSON,
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: res => {
+        // 声明返回的数值
+        var resData = res.data
+        // 初始化数据集合
+        var homeImg = []
+        // 遍历返回数组里面的数据
+        for (let i in resData) {
+          // 暂存每个数据为对象  
+          let temp = {
+            id: resData[i].advertId,
+            imgUrl: resData[i].advertUrl,
+            jumpLink: resData[i].jumpLink
+          }
+          // 把每个暂存的对象放进初始化的数据里面
+          homeImg.push(temp)
+        }
+        // 把数据设置进data数据
+        _this.setData({
+          homeImg: homeImg
+        })
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    // 重定向this指向，预防回调函数改变this问题
+    var _this = this;
+    // 获取本地缓存内的user_id信息
+    wx.getStorage({
+      key: 'loginInfo',
+      success: function(res) {
+        // 把user_id存入本地data数据
+        _this.setData({
+          user_id: res.data.userId
+        })
+      },
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
+  onReady: function() {
+    //ZX函数执行 请求banner轮播图函数
+    this.reqBanner()
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
